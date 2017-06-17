6442c40aaf03401c9f31a4783daf75360b694fcc
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 14:46:27 -0700

    Add sorting data by timestamp

--- app/Services/ServiceFactory.php
+++ app/Services/ServiceFactory.php
@@ -27,14 +27,25 @@ class ServiceFactory
     public function get($service, $limit = 10)
     {
         if (method_exists($this, $service)) {
-            return $this->{$service}($limit);
+            return $this->sortResponseByTimeStamp(
+                $this->{$service}($limit)
+            );
         }
     }
     
     protected function hackernews($limit = 10)
     {
-        $data =  (new HackerNews($this->client))->get($limit);
+        $data = (new HackerNews($this->client))->get($limit);
         
         return (new HackerNewsTransformer($data))->create();
+    }
+    
+    public function sortResponseByTimeStamp(array $data)
+    {
+        usort($data, function ($a, $b) {
+            return $a['timestamp'] - $b['timestamp'];
+        });
+        
+        return $data;
     }
 }
