b1de7354ca76060b5944d726c897e43ca42a70cd
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 16:30:28 -0700

    Add front end layouts

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,4 @@ use App\Http\Controllers\HomeController;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-$app->get('/home', HomeController::class . ':index');
-
-$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
-    return $response->write(
-        'Welcome To Your Slim App
-        <br><a href="/home">Home</a>
-        <a href="/api">Api</a>'
-    );
-});
+$app->get('/', HomeController::class . ':index');
