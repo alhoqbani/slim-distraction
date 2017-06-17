c10ded8ffd9bd8918b402dc357eb6faf3f932d2f
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-17 16:37:07 -0700

    Add vue js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,21 @@
-const app = new Vue({
+var app = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
     data: {
-        test: 'GOOOOOD'
+        items: [],
+        loading: false
+    },
+    methods: {
+        load (service) {
+            this.loading = true
+
+            axios.get('/api/news/' + service).then((response) => {
+                this.items = response.data
+                this.loading = false
+            })
+        }
+    },
+    mounted () {
+        this.load('hackernews')
     }
-});
+})
